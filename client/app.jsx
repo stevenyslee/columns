19bc2e97d6fac81ab4fe29ec5c971b38dce1e444
Steven Lee <stevenyslee@gmail.com>
2018-12-19 18:47:54 -0800

    Shift column contents left and right

--- client/app.jsx
+++ client/app.jsx
@@ -1,20 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Name from './name.jsx';
-
-const elementsSymbol = [ "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt" ];
+import Columns from './columns.jsx'
  
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.shiftLeft = this.shiftLeft.bind(this);
+    this.shiftRight = this.shiftRight.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      1: [{id: 1, person: "A"}, {id: 5, person: "E"}],
+      2: [{id: 2, person: "B"}],
+      3: [{id: 3, person: "C"}, {id: 6, person: "F"}],
+      4: [{id: 4, person: "D"}]
+    });
+    // axios.get('url').then((response) => {
+    //   console.log(response);
+    // }).catch((error) => {
+    //   console.log(error);
+    // });
+  }
+
+  shiftLeft(e) {
+    this.setState((state, props) => {
+      return { 
+        [e - 1]: state[e - 1].concat(state[e]),
+        [e]: [],
+        };
+    });
+  }
+
+  shiftRight(e) {
+    this.setState((state, props) => {
+      let next = Number(e) + 1;
+      console.log(state);
+      return { 
+        [next]: state[next].concat(state[e]),
+        [e]: [],
+        };
+    });
+  }
+
   render() {
-    return (<div>
-      <form>
-      <label>
-        <input type="text" name="name" />
-      </label>
-      </form>
-      <Name line={"Test1"} />
-      <Name line={"Test2"} />
-    </div>);
+    return (
+      <div id="board">
+        {Object.keys(this.state).map((column, id) => {
+            if (!id) {
+              return <Columns key={column} column={column} people={this.state[column]} shiftLeft={null} shiftRight={this.shiftRight} />
+            } else if (id === Object.keys(this.state).length - 1) {
+              return <Columns key={column} column={column} people={this.state[column]} shiftLeft={this.shiftLeft} shiftRight={null} />
+            } else {
+              return <Columns key={column} column={column} people={this.state[column]} shiftLeft={this.shiftLeft} shiftRight={this.shiftRight} />
+            }
+          }, this)}
+          }
+      </div>
+    );
   }
 }
  
